27ba15fc7e58a03740402cc7632d74782a386317
leviongit <levi.duncan.self@gmail.com>
2022-09-28 09:39:05 +0200

    add a manifest file

--- require.rb
+++
@@ -1,5 +0,0 @@
-require "lib/binarydragon/enum.rb"
-require "lib/binarydragon/utils.rb"
-require "lib/binarydragon/data_tag.rb"
-require "lib/binarydragon/serializable.rb"
-require "lib/binarydragon/deserialize.rb"
